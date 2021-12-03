542edfda311afe367a086ce998ae5738fae2f5fe
Naresh <naresh.khatri2345@gmail.com>
2021-12-03 22:20:08 +0530

    Updates

---
+++ precache-manifest.9a7da97964d421a6599711bef9932ad3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4f93ee69ad879d38a0496965724026",
+    "url": "/Naresh-Khatri/donation.git/index.html"
+  },
+  {
+    "revision": "8cf296e68cb91ec61d4c",
+    "url": "/Naresh-Khatri/donation.git/static/js/2.3b18326c.chunk.js"
+  },
+  {
+    "revision": "ea2e67682ff58355750c2a960c8ac43e",
+    "url": "/Naresh-Khatri/donation.git/static/js/2.3b18326c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50d98b5d2d32b49360a",
+    "url": "/Naresh-Khatri/donation.git/static/js/main.15fa4370.chunk.js"
+  },
+  {
+    "revision": "70167c7e082671dbaa6f",
+    "url": "/Naresh-Khatri/donation.git/static/js/runtime-main.f46aff53.js"
+  },
+  {
+    "revision": "e9830f808f3e55f8eb0eeeb23b2b6669",
+    "url": "/Naresh-Khatri/donation.git/static/media/1.e9830f80.jpeg"
+  },
+  {
+    "revision": "0feff2692ec3ed795a0ad0b678a9729e",
+    "url": "/Naresh-Khatri/donation.git/static/media/2.0feff269.jpeg"
+  },
+  {
+    "revision": "9478ca9239c02d44ca16f946b75635c0",
+    "url": "/Naresh-Khatri/donation.git/static/media/3.9478ca92.jpeg"
+  },
+  {
+    "revision": "7d04057f07796edaa9cca06103c7430b",
+    "url": "/Naresh-Khatri/donation.git/static/media/4.7d04057f.jpeg"
+  },
+  {
+    "revision": "2be5f95480e4c91007060cdeb39aa9bf",
+    "url": "/Naresh-Khatri/donation.git/static/media/co_founder.2be5f954.jpeg"
+  },
+  {
+    "revision": "bba8528665725ab04bfdc5afb29c445a",
+    "url": "/Naresh-Khatri/donation.git/static/media/founder.bba85286.jpeg"
+  },
+  {
+    "revision": "3935f00f25f4af28edae82da8446bd51",
+    "url": "/Naresh-Khatri/donation.git/static/media/manager.3935f00f.jpeg"
+  },
+  {
+    "revision": "1a5c701a3affbdff638c6bae9774a027",
+    "url": "/Naresh-Khatri/donation.git/static/media/pic1.1a5c701a.jpeg"
+  },
+  {
+    "revision": "d7d194c8b05e2ea4b5a8bae39d60f365",
+    "url": "/Naresh-Khatri/donation.git/static/media/poor1.d7d194c8.jpg"
+  },
+  {
+    "revision": "3e4928e137f3d0f8b8e68d5e9dc9f7e7",
+    "url": "/Naresh-Khatri/donation.git/static/media/poor2.3e4928e1.jpg"
+  },
+  {
+    "revision": "2cb86246f369747e7e926632ceec477e",
+    "url": "/Naresh-Khatri/donation.git/static/media/poor3.2cb86246.jpg"
+  },
+  {
+    "revision": "f902075f8e9e284c10fc14bde5ddf43d",
+    "url": "/Naresh-Khatri/donation.git/static/media/poor5.f902075f.jpg"
+  },
+  {
+    "revision": "955147ebf3fc67330532e8decd9e5532",
+    "url": "/Naresh-Khatri/donation.git/static/media/qrcode.955147eb.jpeg"
+  }
+]);
